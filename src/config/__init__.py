4988b112f21089b26b018683d4e6b8497817a4a5
Marcelo Bruno Capeletti <marcelocapeletti@gmail.com>
2025-01-30 11:33:31 -0300

    feat: add config init file

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,17 +2,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    APP_KEY : str = '38333295000'
-    APP_SECRET: str = 'fed2163e2e8dccb53ff914ce9e2f1258'
-    BASE_URL: str = 'https://app.omie.com.br/api/v1/'
+    APP_KEY : str 
+    APP_SECRET: str 
+    BASE_URL: str 
 
-    DB_HOST : str = 'vitally-feasible-mongoose.data-1.use1.tembo.io'
-    DB_PORT : int = 5432
-    DB_USERNAME : str = 'postgres'
-    DB_PASSWORD : str = 'oc7Op8zo0c3IqfS1'
-    DB_NAME : str = 'postgres'
+    DB_HOST : str 
+    DB_PORT : int 
+    DB_USERNAME : str 
+    DB_PASSWORD : str 
+    DB_NAME : str 
     class Config:
         env_file='.env'
         env_file_encondig='utf-8'
         extra='ignore'
-
